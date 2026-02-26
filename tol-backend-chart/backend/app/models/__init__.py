e49a75a8b4de9087519983897f320e413e3980eb
Tarik LARBI <tlarbi@gmail.com>
2026-02-26 23:03:29 +0000

    Update

--- tol-backend-chart/backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.document import Document
-from app.models.pipeline_job import PipelineJob
-from app.models.test_case import TestCase, TestRun
-from app.models.query_log import QueryLog
